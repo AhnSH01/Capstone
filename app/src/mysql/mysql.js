a6102f78bc97ae2dcd43ccb88789c8f901c09b0f
AhnSH01 <tlgud9903@naver.com>
2024-10-23 13:48:17 +0900

    mysql 연결 정보 제외

--- app/src/mysql/mysql.js
+++
@@ -1,9 +0,0 @@
-var mysql = require('mysql');
-var conn = mysql.createConnection({
-  host: 'localhost',
-  user: 'capstone',
-  password: '3wjs3qhr',
-  database: 'capstone'
-});
-
-module.exports = conn;
