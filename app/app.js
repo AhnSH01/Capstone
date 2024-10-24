92d2760d5bc3eb9ef461fddda1ed486213b91ad4
AhnSH01 <tlgud9903@naver.com>
2024-10-24 12:05:20 +0900

    dotenv를 사용한 환경변수 설정

--- app/app.js
+++ app/app.js
@@ -4,12 +4,19 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const dotenv = require("dotenv");
-dotenv.config();
+const morgan = require("morgan");
+const fs = require("fs");
 
 const app = express();
+dotenv.config();
 
 // 라우팅
 const home = require("./src/routes/home");
+
+const accessLogStream = fs.createWriteStream(
+    `${__dirname}/log/access.log`,
+    { flags: "a" }
+);
 
 
 app.set("views", "./app/src/views");
@@ -18,7 +25,8 @@ app.use(express.static(`${__dirname}/src/public`));
 app.use(bodyParser.json());
 
 // URL을 통해 전달되는 데이터에 한글, 공백 등과 같은 문자가 포함될 경우 제대로 인식되지 않는 문제 해결
-app.use(bodyParser.urlencoded({ extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(morgan('dev', { stream: accessLogStream }));
 
 app.use("/", home);
 
